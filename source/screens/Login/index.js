d16b8ee7516fc578a2f85ff5f0afa5ff87c6ce4b
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-09-24 16:56:35 -0500

    [ADDED] Login Screen Components

--- source/screens/Login/index.js
+++ source/screens/Login/index.js
@@ -1,15 +1,60 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import Colors from '../../global/colorScheme';
+import {
+  GoogleSignin,
+  statusCodes,
+  GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
 
 const Login = ({navigation}) => {
+  React.useEffect(() => {
+    GoogleSignin.configure({
+      androidClientId: '',
+    });
+  }, []);
+
+  const singIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log({userInfo});
+      /*setloggedIn(true);
+      setUser({userInfo});*/
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (f.e. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.items}>
+        <Image
+          source={require('../../../assets/login/login.png')}
+          style={styles.image}
+        />
         <Text style={styles.title}>Login rápido e fácil</Text>
         <Text style={styles.text}>
           Clique no botão de login pelo Google para acessar rapidamente o app.
+          Seus dados estão seguros e não são compartilhados com terceiros.
         </Text>
+        <GoogleSigninButton
+          style={styles.googleButton}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Light}
+          onPress={() => {
+            singIn();
+          }}
+          disabled={false}
+        />
       </View>
     </View>
   );
@@ -26,6 +71,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 320,
+    height: 320,
+    marginVertical: 32,
+  },
   text: {
     color: 'rgba(255, 255, 255, 0.8)',
     textAlign: 'center',
@@ -35,6 +85,9 @@ const styles = StyleSheet.create({
     color: 'white',
     textAlign: 'center',
     fontWeight: 'bold',
+  },
+  googleButton: {
+    margin: 20,
   },
 });
 
