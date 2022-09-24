80ead7fe01b6218a226896d9358d116aa93b6a19
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-09-24 16:38:45 -0500

    [ADDED] New Screen and configs

--- source/routes.js
+++ source/routes.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import {Linking, TouchableOpacity} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-//import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Colors from './global/colorScheme';
 
 import Intro from './screens/Intro';
 import Login from './screens/Login';
@@ -14,10 +17,46 @@ const Routes = () => {
       <Stack.Navigator
         initialRouteName="Intro"
         screenOptions={{
-          headerShown: false,
+          headerShown: true,
         }}>
-        <Stack.Screen name="Intro" component={Intro} />
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen
+          name="Intro"
+          component={Intro}
+          options={({navigation}) => ({
+            headerStyle: {backgroundColor: Colors.whitetheme.primary},
+            headerTransparent: true,
+            headerTitle: 'D | Walt Gestão',
+            headerTitleAlign: 'center',
+            headerTitleStyle: {color: 'white'},
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => {
+                  Linking.openURL('https://geeknote.devluar.com/ajuda');
+                }}>
+                <Icon name="help" size={30} color="#fff" />
+              </TouchableOpacity>
+            ),
+          })}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={({navigation}) => ({
+            headerStyle: {backgroundColor: Colors.whitetheme.primary},
+            headerTransparent: false,
+            headerTitle: 'Realize o login',
+            headerTitleAlign: 'center',
+            headerTitleStyle: {color: 'white'},
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => {
+                  Linking.openURL('https://geeknote.devluar.com/ajuda');
+                }}>
+                <Icon name="help" size={30} color="#fff" />
+              </TouchableOpacity>
+            ),
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- source/screens/Login/index.js
+++ source/screens/Login/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import Colors from '../../global/colorScheme';
 
 const Login = ({navigation}) => {
   return (
-    <View style={{flex: 1}}>
-      <Text>Done</Text>
+    <View style={styles.container}>
+      <View style={styles.items}>
+        <Text style={styles.title}>Login rápido e fácil</Text>
+        <Text style={styles.text}>
+          Clique no botão de login pelo Google para acessar rapidamente o app.
+        </Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.whitetheme.primary,
+    padding: 50,
+  },
+  items: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'rgba(255, 255, 255, 0.8)',
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: 22,
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+});
 
 export default Login;
